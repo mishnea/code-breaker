df79d7fa02a253901295fa8db716fde5aeec6d53
Mihnea Clejan <contact@mihneaclejan.com>
2026-02-23 21:20:05 +0000

    Lil refactor

--- code-breaker.py
+++ code-breaker.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from readchar import readchar
 import os
 import random
@@ -16,6 +18,10 @@ def bad(s):
     return colorama.Fore.RED + s + colorama.Fore.RESET
 
 
+def info(s):
+    return colorama.Fore.BLUE + s + colorama.Fore.RESET
+
+
 def invert(s):
     return colorama.Back.WHITE + colorama.Fore.BLACK + s + colorama.Style.RESET_ALL
 
@@ -29,53 +35,72 @@ def clear():
 
 class Game:
     def __init__(self, rows):
-        self.word = random.choice(words)
-        self.width = 20
-        weights = [1 + self.word.count(c) for c in ascii_lowercase]
-        self.bottom_row = random.choices(ascii_lowercase, weights, k=len(self.word))
-        self.matrix = [self.genrow() for _ in range(rows)]
+        self.target = random.choice(words)
+        self.width = 21
+        weights = [1 + self.target.count(c) for c in ascii_lowercase]
+        self.player_row = random.choices(ascii_lowercase, weights, k=len(self.target))
+        self.random_rows = [self.genrow() for _ in range(rows)]
         self.attempts = 0
 
     @property
-    def remaining(self):
-        return [d for c, d in zip(self.bottom_row, self.word) if c != d]
-    
+    def remaining_chars(self):
+        return [
+            target_c
+            for player_c, target_c in zip(self.player_row, self.target)
+            if player_c != target_c
+        ]
+
     def genrow(self):
-        weights = [1 + self.remaining.count(c) for c in ascii_lowercase]
-        return random.choices(ascii_lowercase, weights, k=len(self.word))
+        weights = [1 + self.remaining_chars.count(c) for c in ascii_lowercase]
+        return "".join(
+            random.choices(ascii_lowercase, weights, k=1)[0]
+            for _ in range(len(self.target))
+        )
+
+    def center(self, s, char=" "):
+        pad_length = int((self.width + 1) / 2 - len(self.target))
+        pad = char * pad_length
+        return pad + s + pad
 
     def print(self):
         clear()
         # Score row
         print(invert(f"Attempts: {self.attempts}".center(self.width)))
         # Scrambled rows
-        print(
-            *(" ".join(row).center(self.width) for row in self.matrix[::-1]),
-            sep="\n",
-        )
+        for row in self.random_rows[::-1]:
+            formatted = []
+            for index, c in enumerate(row):
+                if c == self.target[index]:
+                    formatted.append(info(c))
+                else:
+                    formatted.append(c)
+            print(self.center(" ".join(formatted)))
         # Player row
         formatted = " ".join(
-            good(c) if c == d else bad(c) for c, d in zip(self.bottom_row, self.word)
+            good(c) if c == d else bad(c) for c, d in zip(self.player_row, self.target)
         )
-        print(formatted.center(len(formatted) - 2 * len(self.bottom_row) + self.width))
+        print(self.center(formatted))
         # Target row
-        print(invert(" ".join(list(self.word)).center(self.width)))
+        print(invert(" ".join(list(self.target)).center(self.width)))
 
     def replace(self, r):
+        if r == "\n" or r == " ":
+            self.player_row = list(self.random_rows[0])
+            return True
         replaced = False
-        for i, (c, d) in enumerate(zip(self.bottom_row, self.matrix[0])):
+        for i, (c, d) in enumerate(zip(self.player_row, self.random_rows[0])):
             if c == r:
                 replaced = True
-                self.bottom_row[i] = d
+                self.player_row[i] = d
         return replaced
 
     def shift(self):
-        self.matrix = self.matrix[1:]
-        self.matrix.append(self.genrow())
+        self.random_rows = self.random_rows[1:]
+        self.random_rows.append(self.genrow())
 
     def start(self):
+        self.print()
         while True:
-            self.print()
             c = readchar()
             if c == "\x1b":
                 # escape pressed
@@ -85,8 +110,8 @@ class Game:
                 continue
             self.shift()
             self.attempts += 1
-            if "".join(self.bottom_row) == "".join(self.word):
-                self.print()
+            self.print()
+            if "".join(self.player_row) == "".join(self.target):
                 print("You wonnered")
                 break
 
